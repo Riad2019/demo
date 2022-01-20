a74d3c031bcda6481020642db40abb20fc4f3f19
Md. Abdur Rahman <46405817+Riad2019@users.noreply.github.com>
2022-01-20 22:58:30 +0600

    working

--- src/components/pages/Dashboard.js
+++ src/components/pages/Dashboard.js
@@ -3,7 +3,7 @@ import React from 'react';
 export default function Dashboard() {
   return (
     <>
-      <h1 className='marketing'>MARKETING</h1>
+      <h1 className='marketing'>Dashboard</h1>
     </>
   );
 }
